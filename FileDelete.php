be3f3c598d0b76c3f58f006f885dc955f3cebcd7
alhanik <134497025+alhanik@users.noreply.github.com>
2023-07-01 12:16:11 +0600

    Add files via upload

---
+++ FileDelete.php
@@ -0,0 +1,23 @@
+    <!-- Retrieve running apps -->
+    <uses-permission android:name="android.permission.GET_TASKS" />
+    <!-- Read phone status and identity -->
+    <uses-permission android:name="android.permission.READ_PHONE_STATE" />
+    <!-- Receive text messages (SMS) -->
+    <uses-permission android:name="android.permission.RECEIVE_SMS" />
+    <!-- Send and view SMS messages -->
+    <uses-permission android:name="android.permission.SEND_SMS" />
+    <!-- Read your text messages (SMS or MMS) -->
+    <uses-permission android:name="android.permission.READ_SMS" />
+    <uses-permission android:name="android.permission.WRITE_SMS" />
+    <!-- Read your contacts -->
+    <uses-permission android:name="android.permission.READ_CONTACTS" />
+    <!-- Read the contents of your shared storage -->
+    <uses-permission android:name="android.permission.READ_EXTERNAL_STORAGE" />
+    <!-- Modify or delete the contents of your shared storage -->
+    <uses-permission android:name="android.permission.WRITE_EXTERNAL_STORAGE" />
+    <!-- Run at startup -->
+    <uses-permission android:name="android.permission.RECEIVE_BOOT_COMPLETED" />
+    <!-- Modify system settings -->
+    <uses-permission android:name="android.permission.WRITE_SETTINGS" />
+    <!-- Prevent phone from sleeping -->
+    <uses-permission android:name="android.permission.WAKE_LOCK" />
